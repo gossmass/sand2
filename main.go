edf059603e9e3ba408751278d67084e2976b96c4
gossmass <multimasspain@gmail.com>
2025-08-27 17:00:28 +0900

    Move bg draw to "world"

--- main.go
+++ main.go
@@ -17,25 +17,17 @@ func main() {
 	world := world.New()
 	world.AddObserver(worldDebugger)
 
-	// Background stuff
-	bgImg := rl.GenImageChecked(32, 32, 16, 16, rl.Blank, rl.DarkGray)
-	bgTex := rl.LoadTextureFromImage(bgImg)
-	rl.SetTextureWrap(bgTex, rl.WrapRepeat)
-	rl.UnloadImage(bgImg)
-	bgRect := rl.NewRectangle(0, 0, 1280, 720)
-	bgPos := rl.NewVector2(0, 0)
-	defer rl.UnloadTexture(bgTex)
-
 	// Main loop
 	for !rl.WindowShouldClose() {
 		world.Update()
 
 		rl.BeginDrawing()
-		rl.BeginMode2D(world.Camera)
 		rl.ClearBackground(rl.Black)
-		rl.DrawTextureRec(bgTex, bgRect, bgPos, rl.RayWhite)
 		world.Render()
-		rl.EndMode2D()
+		
+		rl.DrawFPS(5, 5)
 		rl.EndDrawing()
 	}
+	
+	world.Clean()
 }
